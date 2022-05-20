33d20735cbc4ae1165232c20fc0bdf282a66a3b1
Ol4ik88 <slapikol4ik@gmail.com>
2022-05-21 02:01:18 +0300

    feat: implement task-3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,14 +2,19 @@ const path = require('path');
 const { stat, readdir} = require('fs/promises');
 
 const dir = path.join(__dirname, 'secret-folder');
-const files = readdir(dir, { withFileTypes: true });
 
-files.then((file) => {
-  const onlyFiles = file.filter((allFiles) => allFiles.isFile());
-  for (let allFiles of onlyFiles){
-    stat(path.join(dir, allFiles.name)).then((fileStats) => {
-      const file = path.parse(allFiles.name);
-      process.stdout.write(`${file.name} - ${file.ext.slice(1)} - ${(fileStats.size/1024).toFixed(3)} kb\n`);
-    });
+async function infoOfFile (){
+  const files = await readdir(dir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isFile()) {
+      const fileStats = await stat(path.join(dir, file.name));
+      const info = path.parse(file.name);
+      process.stdout.write(`${info.name} - ${info.ext.slice(1)} - ${(fileStats.size/1024).toFixed(3)} kb\n`);
+    }
   }
-});
+}
+try {
+  infoOfFile();
+} catch (err) {
+  process.stderr.write(err.message);
+}
