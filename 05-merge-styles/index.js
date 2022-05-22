113619ece5faa33d8792bc88b5a7abb3e0515ebe
Ol4ik88 <slapikol4ik@gmail.com>
2022-05-23 00:38:12 +0300

    refactor: change comparison in function bundle

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,15 +8,12 @@ const dirTarget = path.join(__dirname, 'project-dist', 'bundle.css');
 const ws = fs.createWriteStream(dirTarget);
 
 async function bundle() {
-  const dirent = await readdir(dirSource, { withFileTypes: true });
-
-  for (const file of dirent) {
-    if (file.isFile()) {
+  const files = await readdir(dirSource, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isFile() && path.parse(file.name).ext === '.css') {
       const fileCSS = path.join(dirSource, file.name);
-      if (path.extname(fileCSS) === '.css') {
-        const rs = fs.createReadStream(fileCSS);
-        rs.pipe(ws, {end: false});
-      }
+      const rs = fs.createReadStream(fileCSS);
+      rs.pipe(ws, {end: false});
     }
   }
 }
